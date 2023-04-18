99e922aa1c1580635cf23cf41e4f7641e940ae97
Jay Wolff <jaywolff16@gmail.com>
2023-04-18 11:31:37 -0400

    merge missing change

--- app/services/rad_common/sms/incoming_processor.rb
+++ app/services/rad_common/sms/incoming_processor.rb
@@ -20,7 +20,7 @@ module RadCommon
 
       def process
         @command_results = mms? ? process_mms : process_sms
-        log_sms!
+        log_sms! unless mms?
         @sms_reply = @command_results.sms_reply
         return unless @command_results.reply
 
@@ -76,8 +76,6 @@ module RadCommon
         end
 
         def log_sms!
-          return if mms?
-
           @log = TwilioLog.create! to_number: RadicalConfig.twilio_phone_number!,
                                    from_number: @phone_number,
                                    message: @incoming_message
