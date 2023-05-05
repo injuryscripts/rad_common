6471edf1c09f051148c0ecbab3c8e468aa2c928e
Jay Wolff <jaywolff16@gmail.com>
2023-05-05 12:58:21 -0400

    dont blow up when sms/mms is sent to unsupported country

--- app/services/phone_sms_sender.rb
+++ app/services/phone_sms_sender.rb
@@ -28,7 +28,7 @@ class PhoneSMSSender
     true
   rescue Twilio::REST::RestError => e
     self.exception = e
-    raise e.message unless blacklisted?
+    raise e.message unless blacklisted? || unsupported_country?
 
     log_event false
     handle_blacklist
@@ -37,6 +37,17 @@ class PhoneSMSSender
 
   def from_number
     mms? ? twilio.from_number_mms : twilio.from_number
+  end
+
+
+  def blacklisted?
+    # https://www.twilio.com/docs/api/errors/21610
+    exception.message.include?('21610')
+  end
+
+  def unsupported_country?
+    # https://www.twilio.com/docs/api/errors/21408
+    exception.message.include? '21408'
   end
 
   private
@@ -63,11 +74,6 @@ class PhoneSMSSender
 
     def to_number
       RadicalTwilio.human_to_twilio_format(to_mobile_phone)
-    end
-
-    def blacklisted?
-      # https://www.twilio.com/docs/api/errors/21610
-      exception.message.include?('21610')
     end
 
     def opt_out_message_already_sent?
