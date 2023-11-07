7838dadb236d2ad32149b2716c8a92a84816f39d
Jay Wolff <jaywolff16@gmail.com>
2023-11-07 09:43:06 -0500

    on user account confirmation, accept invitation if hasnt been accepted already

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -3,6 +3,7 @@ module Users
     def show
       super
       Notifications::NewUserSignedUpNotification.main.notify!(resource) if resource.errors.empty?
+      resource.accept_invitation! if resource.invitation_accepted_at.nil?
     end
   end
 end
