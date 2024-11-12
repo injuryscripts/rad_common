8d5ab0c1c4303b90ebd51e34bbe8aa47e8fdf717
Jay Wolff <jaywolff16@gmail.com>
2024-11-12 16:24:03 -0500

    make global super search optional

--- spec/rad_common/system/searches_spec.rb
+++ spec/rad_common/system/searches_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Searches', type: :system do
 
   before { login_as admin, scope: :user }
 
-  context 'with super search' do
+  context 'with super search', :js do
     let(:term) { 'Peters' }
 
     let(:prompt) do
@@ -16,21 +16,36 @@ RSpec.describe 'Searches', type: :system do
 
     before { create(:user, last_name: term) }
 
-    context 'when asking the user if they want to use' do
-      it 'clears checkbox if dismissed', js: true do
-        visit '/'
-        page.dismiss_confirm prompt do
-          check 'super_search'
-        end
-        expect(first('.global_search_name', match: :first)[:placeholder]).to eq 'Search user by name'
-      end
+    context 'when super search is enabled' do
+      before { allow(RadConfig).to receive(:enable_super_search?).and_return(true) }
 
-      it 'uses if confirmed', js: true do
-        visit '/'
-        page.accept_confirm prompt do
-          check 'super_search'
+      context 'when asking the user if they want to use' do
+        it 'clears checkbox if dismissed' do
+          visit '/'
+          expect(page).to have_css 'input.super_search'
+          page.dismiss_confirm prompt do
+            check 'super_search'
+          end
+          expect(first('.global_search_name', match: :first)[:placeholder]).to eq 'Search user by name'
         end
-        expect(first('.global_search_name')[:placeholder]).to eq 'Super Search'
+
+        it 'uses if confirmed' do
+          visit '/'
+          expect(page).to have_css 'input.super_search'
+          page.accept_confirm prompt do
+            check 'super_search'
+          end
+          expect(first('.global_search_name')[:placeholder]).to eq 'Super Search'
+        end
+      end
+    end
+
+    context 'when super search is not enabled' do
+      before { allow(RadConfig).to receive(:enable_super_search?).and_return(false) }
+
+      it 'does not have a super search option' do
+        visit '/'
+        expect(page).not_to have_css 'input.super_search'
       end
     end
   end
